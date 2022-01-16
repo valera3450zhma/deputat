49188d69da63140f1c8b4bec72f8e9fb87aaa99d
rainett <96131229+rainett@users.noreply.github.com>
2022-01-16 11:36:54 +0200

    try

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 
 import config
 import res
+import deputat
 
 import telebot
 import logging
@@ -26,36 +27,7 @@ def start_handler(message):
 
 @bot.message_handler(commands=['get'])
 def get_deputat_handler(message):
-    user_id = message.from_user.id
-    db_object.execute(f"SELECT userid, deputatid FROM deputats WHERE userid = {user_id}")
-    result = db_object.fetchone()
-    db_object.execute("SELECT deputatid FROM deputats WHERE deputatid IS NOT NULL ORDER BY deputatid DESC LIMIT 1")
-    last_deputat = db_object.fetchone()
-    if result is None:
-        if last_deputat is None:
-            deputat_id = 1
-        else:
-            deputat_id = last_deputat[0] + 1
-        db_object.execute("INSERT INTO deputats(userid, money, name, level, photo, username, deputatid, rating) VALUES "
-                          "( %s, %s, %s, %s, %s, %s, %s, %s)",
-                          (user_id, random.randint(10, 100), random.choice(res.deputatNames), 1,
-                           random.randint(0, len(res.level_photos[0]) - 1),
-                           message.from_user.first_name, deputat_id, 0))
-        db_connection.commit()
-        bot.reply_to(message, "Осьо! Ваш перший дєпутат! Позирити на нього - /show")
-    elif result[1] is None:
-        if last_deputat is None:
-            deputat_id = 1
-        else:
-            deputat_id = last_deputat[0] + 1
-        db_object.execute("UPDATE deputats SET deputatid = %s, money = %s, name = %s, level = %s,  photo = %s, "
-                          "rating = %s WHERE userid = %s", (deputat_id, random.randint(10, 100), random.choice(
-                           res.deputatNames), 1, random.randint(0, len(res.level_photos[0]) - 1), 0, result[0]))
-        db_connection.commit()
-        bot.reply_to(message, "Гля який! Депута-а-а-атіще! Глянуть на підарасіка - /show")
-    else:
-        bot.reply_to(message, "В тебе вже є депутат")
-        bot.send_sticker(message.chat.id, res.what_sticker)
+    deputat.Deputat.get_deputat(message, db_object, db_connection, bot)
 
 
 @bot.message_handler(commands=['show'])
