dd6b5c87cebab5d578e39f083990de16bad9863a
rainett <96131229+rainett@users.noreply.github.com>
2022-01-27 19:07:56 +0200

    try

--- deputat.py
+++ deputat.py
@@ -138,8 +138,9 @@ def start_election(message, db_object, db_connection, bot, chat_id):
         optionss = []
         for ress in result:
             text = ress[1] + ' (' + ress[0] + ')'
+            bot.send_message(message.chat.id, text)
             optionss.append(text)
-        bot.send_poll(message.chat.id, "Вибирай давай", optionss[1:])
+        bot.send_poll(message.chat.id, "Вибирай давай", optionss[1:], is_anonymous=False)
 
 
 def _show_candidates_(call, db_object, db_connection, bot, chat_id):
